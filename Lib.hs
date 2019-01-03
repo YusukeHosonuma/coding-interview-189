8b00b15531e5a9845e8d0f975043353565a64283
Yusuke Hosonuma <tobi462@gmail.com>
2019-01-03 19:39:49 +0900

    add: Q1

---
+++ Lib.hs
@@ -0,0 +1,168 @@
+-- 標準ライブラリの関数を書き直したモジュール
+module Lib where
+
+import Data.List
+
+-- | 否定
+--
+-- >>> not True
+-- False
+-- >>> not False
+-- True
+--
+not' :: Bool -> Bool
+not' True  = False
+not' False = True
+
+-- | リストに要素が含まれているか判定（`elem`の独自実装）
+--
+-- >>> elem' 1 []
+-- False
+-- >>> elem' 1 [1, 2]
+-- True
+-- >>> elem' 3 [1, 2]
+-- False
+--
+-- 標準と比較して検証：
+-- prop> elem' x xs == elem x xs
+--
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' x (y:ys) = x == y || elem' x ys
+
+-- | `even`の独自実装
+--
+-- >>> even' 1
+-- False
+-- >>> even' 2
+-- True
+--
+-- 標準と比較して検証：
+-- prop> even' x == even x
+--
+even' :: (Integral a) => a -> Bool
+even' a = a `mod` 2 == 0
+
+-- | `filter`の独自実装
+--
+-- >>> filter' (\_ -> True) []
+-- []
+-- >>> filter' (\x -> x == 1) [1]
+-- [1]
+-- >>> filter' (\x -> x == 1) [1, 2]
+-- [1]
+-- >>> filter' (\x -> x == 1) [2, 1]
+-- [1]
+-- >>> filter' (\x -> x == 1) [1, 1, 2]
+-- [1,1]
+--
+-- 標準と比較して検証：
+-- prop> filter' (\x -> even' x) xs == filter (\x -> even' x) xs
+--
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs) = if f x
+    then x : ys
+    else ys
+        where ys = filter' f xs
+
+-- | リストのソート（`sort`の独自実装）
+--
+-- >>> sort' []
+-- []
+-- >>> sort' [1]
+-- [1]
+-- >>> sort' [2, 1]
+-- [1,2]
+-- >>> sort' [1, 1]
+-- [1,1]
+-- >>> sort' [3, 2, 1]
+-- [1,2,3]
+-- >>> sort' [3, 3, 1]
+-- [1,3,3]
+-- >>> sort' [3, 1, 1]
+-- [1,1,3]
+-- >>> sort' [1, 1, 1]
+-- [1,1,1]
+-- >>> sort' [3, 2, 4, 1]
+-- [1,2,3,4]
+--
+-- 標準と比較して検証：
+-- prop> sort' xs == sort xs
+--
+sort' :: (Ord a) => [a] -> [a]
+sort' [] = []
+sort' (x:xs) = ys ++ [x] ++ zs
+    where
+        ys = sort' $ filter (\y -> y <= x) xs
+        zs = sort' $ filter (\z -> z > x) xs
+
+-- | `takeWhile`の独自実装
+--
+-- >>> takeWhile (< 3) []
+-- []
+-- >>> takeWhile (< 3) [1..4]
+-- [1,2]
+--
+-- 標準と比較して検証：
+-- prop> takeWhile' (< 10) xs == takeWhile (< 10) xs
+--
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' f (x:xs) = if f x
+    then x : takeWhile' f xs
+    else []
+
+
+-- | `dropWhile`の独自実装
+--
+-- >>> dropWhile (< 3) []
+-- []
+-- >>> dropWhile (< 3) [1]
+-- []
+-- >>> dropWhile (< 3) [1..4]
+-- [3,4]
+--
+-- 標準と比較して検証：
+-- prop> dropWhile' (< 10) xs == dropWhile (< 10) xs
+--
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' f zs@(x:xs) = if f x
+    then dropWhile' f xs
+    else zs
+
+-- | `span`の独自実装
+--
+-- >>> span' (1 ==) []
+-- ([],[])
+-- >>> span' (1 ==) [1, 1, 1, 2, 3]
+-- ([1,1,1],[2,3])
+--
+-- 標準と比較して検証：
+-- prop> span' (< 3) xs == span (<3) xs
+--
+span' :: (a -> Bool) -> [a] -> ([a], [a])
+span' f xs = (takeWhile' f xs, dropWhile' f xs)
+
+-- | `group`の独自実装
+--
+-- >>> group' [1]
+-- [[1]]
+-- >>> group' [1, 1]
+-- [[1,1]]
+-- >>> group' [1, 1, 2]
+-- [[1,1],[2]]
+-- >>> group' [1, 2, 2]
+-- [[1],[2,2]]
+-- >>> group' [1, 1, 1]
+-- [[1,1,1]]
+--
+-- 標準と比較して検証：
+-- prop> group' xs == group xs
+--
+group' :: (Eq a) => [a] -> [[a]]
+group' [] = []
+group' (x:xs) = (x:ys) : group' zs
+    where
+        (ys, zs) = span' (x ==) xs
---
+++ Q1.hs
@@ -0,0 +1,32 @@
+-- ある文字列が、全てユニークである（重複する文字がない）かどうかを判定するアルゴリズムを実装してください。
+-- また、それを実装するのに新たなデータ構造が使えない場合、どのようにすればよいですか？
+module Q1 where
+
+import Lib
+
+-- | 文字がすべて異なるか判定する。
+--
+-- >>> isUnique ""
+-- True
+-- >>> isUnique "a"
+-- True
+-- >>> isUnique "ab"
+-- True
+-- >>> isUnique "aa"
+-- False
+-- >>> isUnique "abc"
+-- True
+-- >>> isUnique "aac"
+-- False
+-- >>> isUnique "acc"
+-- False
+-- >>> isUnique "aba"
+-- False
+--
+-- ユニークな文字列は、ソート・グループした文字数の長さと、元の文字列の長さが一致する
+-- prop> isUnique s == ((length $ group' $ sort' s) == length s)
+--
+isUnique :: String -> Bool
+isUnique [] = True
+isUnique [_] = True
+isUnique (x:xs) = not' (x `elem'` xs) && isUnique xs
