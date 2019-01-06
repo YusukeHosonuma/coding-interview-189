51775b4b8c77a6ab5e4455cce0dc45145679df00
Yusuke Hosonuma <tobi462@gmail.com>
2019-01-06 17:49:36 +0900

    clean: style

--- src/Q3.hs
+++ src/Q3.hs
@@ -4,20 +4,20 @@ import Lib
 
 -- | 循環リストの先頭を探す
 --
--- >>> findCycleStart $ [(x, x) | x <- [0..4] ++ cycle [5..9]]
+-- >>> findCycleStart $ [ (x, x) | x <- [0..4] ++ cycle [5..9] ]
 -- 5
--- >>> findCycleStart $ [(x, x) | x <- [0..4] ++ cycle [5..10]]
+-- >>> findCycleStart $ [ (x, x) | x <- [0..4] ++ cycle [5..10] ]
 -- 5
--- >>> findCycleStart $ [(x, x) | x <- [0..5] ++ cycle [6..10]]
+-- >>> findCycleStart $ [ (x, x) | x <- [0..5] ++ cycle [6..10] ]
 -- 6
--- >>> findCycleStart $ [(x, x) | x <- [0..5] ++ cycle [6..11]]
+-- >>> findCycleStart $ [ (x, x) | x <- [0..5] ++ cycle [6..11] ]
 -- 6
--- >>> findCycleStart $ [(x, x) | x <- [0] ++ cycle [1..2]]
+-- >>> findCycleStart $ [ (x, x) | x <- [0] ++ cycle [1..2] ]
 -- 1
--- >>> findCycleStart $ [(x, x) | x <- [0] ++ repeat 1]
+-- >>> findCycleStart $ [ (x, x) | x <- [0] ++ repeat 1 ]
 -- 1
 --
--- prop> x >= 1 && y >= 1 ==> findCycleStart [(x, x) | x <- take x [0..] ++ cycle (take y [x..])] == x
+-- prop> x >= 1 && y >= 1 ==> findCycleStart [ (x, x) | x <- take x [0..] ++ cycle (take y [x..]) ] == x
 --
 findCycleStart :: [(Int, a)] -> a
 findCycleStart []  = errorWithoutStackTrace "list is empty"
