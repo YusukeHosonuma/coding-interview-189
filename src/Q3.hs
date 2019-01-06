5435f1e90155de80dd81b50babb617a167866070
Yusuke Hosonuma <tobi462@gmail.com>
2019-01-06 17:47:15 +0900

    refactor: prefer list comprehension

--- src/Q3.hs
+++ src/Q3.hs
@@ -4,20 +4,20 @@ import Lib
 
 -- | 循環リストの先頭を探す
 --
--- >>> findCycleStart $ zip ([0..4] ++ cycle [5..9])  [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0..4] ++ cycle [5..9]]
 -- 5
--- >>> findCycleStart $ zip ([0..4] ++ cycle [5..10]) [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0..4] ++ cycle [5..10]]
 -- 5
--- >>> findCycleStart $ zip ([0..5] ++ cycle [6..10]) [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0..5] ++ cycle [6..10]]
 -- 6
--- >>> findCycleStart $ zip ([0..5] ++ cycle [6..11]) [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0..5] ++ cycle [6..11]]
 -- 6
--- >>> findCycleStart $ zip ([0] ++ cycle [1..2]) [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0] ++ cycle [1..2]]
 -- 1
--- >>> findCycleStart $ zip ([0] ++ cycle [1]) [0..]
+-- >>> findCycleStart $ [(x, x) | x <- [0] ++ repeat 1]
 -- 1
 --
--- prop> x >= 1 && y >= 1 ==> findCycleStart (zip (take x [0..] ++ (cycle (take y [x..]))) [0..]) == x
+-- prop> x >= 1 && y >= 1 ==> findCycleStart [(x, x) | x <- take x [0..] ++ cycle (take y [x..])] == x
 --
 findCycleStart :: [(Int, a)] -> a
 findCycleStart []  = errorWithoutStackTrace "list is empty"
